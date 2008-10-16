d7b4ee361ceb5f6c6c59ee6c010ae00bad8b76e3
Giacomo Ritucci <giacomo.ritucci@email.it>
2008-10-16 17:01:01 +0200

    Fix test parser
    src/test/ccs-parser-test.lisp:
    
    	* meno confusione.

--- src/test/ccs-parser-test.lisp
+++ src/test/ccs-parser-test.lisp
@@ -8,27 +8,40 @@
 
 
 (define-test is-action
-	     (assert-true (is-action "a"))
-	     (assert-true (is-action "prova"))
-	     (assert-true (is-action "&"))	; tau
-	     (assert-false (is-action "Prova"))
-	     (assert-false (is-action "PROVA")))
+	     (assert-true (is-action "&"))
+	     (assert-false (is-action "!&"))
+	     (assert-false (is-action "&!"))
+	     (assert-false (is-action "&&")))
 
 
 (define-test is-label
+	     (assert-true (is-label "a"))
+	     (assert-true (is-label "!a"))
+	     (assert-true (is-label "label"))
+	     (assert-true (is-label "!label"))
+	     (assert-false (is-label "!!a"))
+	     (assert-false (is-label "!a!"))
+	     (assert-false (is-label "a!"))
+	     (assert-false (is-label "a!!"))
+	     (assert-false (is-label "!!label"))
+	     (assert-false (is-label "!la!bel"))
+	     (assert-false (is-label "!label!"))
 	     (assert-false (is-label "&"))
-	     (assert-true (is-action "prova"))
-	     (assert-true (is-action "&"))	; tau
-	     (assert-false (is-action "Prova"))
-	     (assert-false (is-action "PROVA")))
+	     (assert-false (is-label "*"))
+	     (assert-false (is-label "Label"))
+	     (assert-false (is-label "LABEL"))
+	     (assert-false (is-label "labeL"))
+	     (assert-false (is-label "!labeL"))
+	     (assert-false (is-label "!Label")))
+
 
 (define-test is-process
-	     (assert-true (is-process "Ciao"))
-	     (assert-true (is-process "C"))
-	     (assert-true (is-process "CIAO"))
-	     (assert-false (is-process "ciAO"))	; fallisce, servono regexp
-	     (assert-false (is-process "ciao"))
-	     (assert-false (is-process "&")))
+	     (assert-true (is-process "P"))
+	     (assert-true (is-process "PROCESS"))
+	     (assert-true (is-process "Process"))
+	     (assert-false (is-process "process"))
+	     (assert-false (is-process "pROCESS"))
+	     (assert-false (is-process "!P")))
 
 
 (run-tests)
